5e5cb6c89a4ac2e4f1a08a11ce8e5b46c3e16410
arnaud sellenet <arnodmental@gmail.com>
2014-09-23 15:28:45 +0200

    load correct environment for db

--- my_app.rb
+++ my_app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'mongoid'
 require './models/picking'
 
-Mongoid.load!('config/mongoid.yml', :development)
+Mongoid.load!('config/mongoid.yml', ENV['RACK_ENV'].to_sym)
 
 get '/' do
   'Hello World'
