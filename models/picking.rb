63d1b2f7a3b56c62bc27a08d47b487de76036679
arnaud sellenet <arnodmental@gmail.com>
2014-09-23 15:20:02 +0200

    first attempt of picking creation thru POST

--- models/picking.rb
+++ models/picking.rb
@@ -1,9 +1,9 @@
 module Fungileaks
   class Picking
     include Mongoid::Document
-    field :picked_at, type: TimeWithZone
-    field :username, type: String
-    field :species, type: String
-    field :weight, type: BigDecimal
+    field :picked_at, type: DateTime
+    field :username,  type: String
+    field :species,   type: String
+    field :latlng,    type: String
   end
 end
--- my_app.rb
+++ my_app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'mongoid'
+require './models/picking'
+
+Mongoid.load!('config/mongoid.yml', :development)
 
 get '/' do
   'Hello World'
+end
+
+post '/api/v1/picking' do
+  picking = Fungileaks::Picking.new(params)
+  picking.save!
+  picking.id.inspect
 end
