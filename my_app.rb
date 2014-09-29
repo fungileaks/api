19bf62e4954a4b2eb8bbbbc2cc94459a887f0a56
arnaud sellenet <arnodmental@gmail.com>
2014-09-30 00:30:32 +0200

    POST #create and GET #show

--- my_app.rb
+++ my_app.rb
@@ -13,6 +13,26 @@ module Fungileaks
       get :latest do
         Picking.order_by('picked_at DESC').limit(10).map(&:as_json)
       end
+
+      desc 'Return a picking by its ID'
+      route_param :id do
+        get do
+          Picking.find(params[:id]).as_json
+        end
+      end
+
+      desc 'Create a new picking'
+      params do
+        requires :picked_at, type: String, desc: 'The date you picked those'
+        requires :username, type: String, desc: 'Your desired user name'
+        requires :species, type: String, desc: 'The king of mushroom you picked'
+        requires :latlng, type: String, desc: 'Latitude/longitude, comma separated'
+      end
+      post do
+        picking = Fungileaks::Picking.new declared(params)
+        picking.save!
+        picking.as_json
+      end
     end
   end
 end
